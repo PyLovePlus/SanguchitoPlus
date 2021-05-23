8ee9ac0661dacfa7789ba61eec13bba54c4926ca
vicpaciello <58981648+vicpaciello@users.noreply.github.com>
2021-05-22 23:49:53 -0400

    Función extra 1 - Cant sanguchitos vendidos por tipo

--- estadisticas.py
+++ estadisticas.py
@@ -1,9 +1,22 @@
 #Estadísticas de ventas - Cant de sandwich por tipo vendido y cant total
 
-"""def tipo_vendidos (hist_pedidos: list):
-	i, j = 0
+def tipo_vendidos (hist_pedidos: list):
+	i, j, ind, dob, tri, total = 0
 
-	for i in range(0, len(hist_pedidos)):"""
+	for i in range(0, len(hist_pedidos)):
+		for j in range(0, len(hist_pedidos[i])):
+			if hist_pedidos[i][j] == 'i':
+				ind += 1
+			elif hist_pedidos[i][j] == 'd':
+				dob += 1
+			else:
+				tri += 1
+
+	total = ind + dob + tri
+	print("La cantidad total de 'sanguchitos' vendidos son: ", total)
+	print("La cantidad de 'sanguchitos' de tipo individual vendidos son: ", ind)
+	print("La cantidad de 'sanguchitos' de tipo doble vendidos son: ", dob)
+	print("La cantidad de 'sanguchitos' de tipo triple vendidos son: ", tri)
 
 #Estadisticas de ventas - Ventas por ingredientes ordenados de mayor a menor
 
