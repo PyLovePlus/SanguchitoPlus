4113f2be7f2b49a7afdbc5222dfe85985c4feef5
Ricardo Salvatorelli <rsca4321@gmail.com>
2021-05-27 11:02:22 -0400

    13 Problema de append lista en módulo estadística (#15)
    * 🐛Solucionado bug de iteración en mod estadísticas
    
    + Bug de iteración sobre lista y dicionarios
    
    * Solución de funcionalidad de ingredientes mas vendidos no modular
    
    - Ahora la funcionalidad de ingredientes mas vendidos generará la lista de ingredientes mas vendidos de 
    manera dinámica para que se puedan mostrar ingredientes adicionales a los del enunciado
    
    * Mensaje en caso de histórico de pedidos vacio
    
    Co-authored-by: Miguel <migueldeolim1@gmail.com>

--- estadisticas.py
+++ estadisticas.py
@@ -1,51 +1,62 @@
 #Estadísticas de ventas - Cant de sandwich por tipo vendido y cant total
 
+from pprint import *
+
 def tipo_vendidos (hist_pedidos: list):
 	i = j = ind = dob = tri = total = 0
 
 	for i in range(0, len(hist_pedidos)):
-		for j in range(0, len(hist_pedidos[i])):
-			if hist_pedidos[i][j] == 'i':
-				ind += 1
-			elif hist_pedidos[i][j] == 'd':
-				dob += 1
-			else:
-				tri += 1
+		if hist_pedidos[i]['alias'] == 'i':
+			ind += 1
+		elif hist_pedidos[i]['alias'] == 'd':
+			dob += 1
+		else:
+			tri += 1
 
 	total = ind + dob + tri
-	print("La cantidad total de 'sanguchitos' vendidos son: ", total)
-	print("La cantidad de 'sanguchitos' de tipo individual vendidos son: ", ind)
-	print("La cantidad de 'sanguchitos' de tipo doble vendidos son: ", dob)
-	print("La cantidad de 'sanguchitos' de tipo triple vendidos son: ", tri)
+	print("\tLa cantidad total de 'sanguchitos' vendidos son: ", total)
+	print("\tLa cantidad de 'sanguchitos' de tipo individual vendidos son: ", ind)
+	print("\tLa cantidad de 'sanguchitos' de tipo doble vendidos son: ", dob)
+	print("\tLa cantidad de 'sanguchitos' de tipo triple vendidos son: ", tri)
 
-#Estadísticas de ventas - Ventas por ingredientes ordenados de mayor a menor
 
-def ing_mas_vendidos (hist_pedidos: list):
-	i = j = k = jam = cha = pim = dqs = ace = ppe = sal = 0
+def imprimir_ingredientes_vendidos (ing_vendidos: dict):
+	# definición de __doc__
+	"""
+		Función que imprime en pantalla los ingredientes mas vendidos de manera ordenada
+		Argumento:
+			ing_vendidos: Diccionario de ingredientes vendidos que contiene nombre y cantidad
+	"""
+	# ordenar diccionario
+	ing_vendidos = sorted(ing_vendidos.values(), key=lambda ing: ing["cantidad"], reverse=True)
 
-	for i in range(0, len(hist_pedidos)):
-		for j in range(0, len(hist_pedidos[i])):
-			for k in range(0, len(hist_pedidos[i][j])):
-				if hist_pedidos[i][j][k] == 'ja':
-					jam += 1
-				elif hist_pedidos[i][j][k] == 'ch':
-					cha += 1
-				elif hist_pedidos[i][j][k] == 'pi':
-					pim += 1
-				elif hist_pedidos[i][j][k] == 'dq':
-					dqs += 1
-				elif hist_pedidos[i][j][k] == 'ac':
-					ace += 1
-				elif hist_pedidos[i][j][k] == 'pp':
-					ppe += 1
-				else:
-					sal += 1
+	if len(ing_vendidos) > 0:
+		for ingrediente in ing_vendidos:
+			print("\t{}: {}".format(ingrediente["nombre"], ingrediente["cantidad"]))
+	else:
+		print("¡No se ha realizado ventas!")
 
-	#Ordenar los contadores de ingredientes
-	list_aux = []
+def ing_vendidos (hist_pedidos: list):
+	# definición de __doc__
+	"""
+		Función que genera un diccionario con los ingredientes vendidos y luego
+		los imprime en pantalla ordenados.
+		Argumento:
+			hist_pedidos: lista de pedidos del sistema
+	"""
+	ing_vendidos: dict = {}
+	for pedido in hist_pedidos:
+		for ingrediente in pedido["ingredientes"]:
+			if not ing_vendidos.get(ingrediente["alias"]):
+				ing_vendidos[ingrediente["alias"]] = {
+					"nombre": ingrediente["nombre"],
+					"cantidad": 1
+				}
+			else:
+				ing_vendidos[ingrediente["alias"]]["cantidad"] += 1
+				
+	imprimir_ingredientes_vendidos(ing_vendidos)
+	
 
-	list_aux.append([jam, "Jamón"], [cha, "Champiñones"], [pim, "Pimentón"], [dqs, "Doble Queso"], [ace, "Aceituna"], [ppe, "Pepperoni"], [sal, "Salchichón"])
-	list_aux.sort()
-	print("Los 3 ingredientes más vendidos fueron los siguientes:")
-	for i in range(0, 2):
-			print(i, "-. ", list_aux[i][i+1])
+
+
